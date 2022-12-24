0870bb33a7c8c3d0ff799dcc856338bcd0c009aa
Ozan Ayyıldız <ozanayyildiz92@gmail.com>
2022-12-24 23:25:16 +0300

    this will make the axios work

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 import axios from "axios";
 
 const getData = async (number) => {
-  const user = await axios("https://jsonplaceholder.typicode.com/users/" + number);
-    console.log(user);
+  const user = await axios(
+    "https://jsonplaceholder.typicode.com/users/" + number
+  );
+  console.log(user);
+  const post = await axios(
+    "https://jsonplaceholder.typicode.com/posts?userId=" + number
+  );
+  console.log(post);
 };
 
 export default getData;
